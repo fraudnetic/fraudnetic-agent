94fe7256169aa1b57a182693369d913b6adf7360
Davit G. <uxcsoftware@gmail.com>
2023-10-04 16:28:34 +0400

    new agent with additional data

--- library/agent.class.d.ts
+++ library/agent.class.d.ts
@@ -1,14 +1,13 @@
 export default class Agent {
     private static getAvailableResolution;
     private static getColorDepth;
-    private static getCurrentResolution;
+    static getCurrentResolution(): string;
     private static getScreenPrint;
     private static getFontsList;
     static getCanvasFingerprint(): string;
     static getFontsFingerprint(): number;
     static getWebGLFingerprint(): number;
     static isIncognito(): Promise<boolean>;
-    static getUserAgent(): string;
     static getPeriodicWaveFingerprint(): Promise<number>;
     static getBrowserFingerprint(): Promise<number>;
 }
